713dfd8d90615a3758ad2671e07bad279282ab79
Eyub197 <eyubmehmed122@gmail.com>
2023-07-12 15:39:10 +0300

    Rules of the game

--- app.js
+++ app.js
@@ -11,3 +11,34 @@ let getPlayerChoice = () => {
     let choice = prompt("Choose rock paper or scissors :")
     return choice
 }
+
+export let rules = (computerChoice = getComputerChoice() , playerChoice = getPlayerChoice()) => {
+    let message
+    if(
+    (computerChoice === "rock" && playerChoice === "scissors") || 
+    (computerChoice === "paper" &&  playerChoice === "rock")   ||
+    (computerChoice === "scissors" && playerChoice === "paper")){
+        message = "You lost"
+    } else if (computerChoice === playerChoice){
+        message = "Tie"
+    }else{
+        message = "You win"
+    }
+    
+    return message
+}
+
+export let determinateWinner = (computerChoice = getComputerChoice(), playerChoice = getPlayerChoice()) => {
+    let result = rules(computerChoice, playerChoice)
+    let winner
+
+    if(result === "You lost"){
+        winner = "computer"
+        return winner
+    }else if (result === "Tie"){
+         winner = "tie"
+         return winner}
+    else{
+        winner = "you"
+        return winner
+    }
